b0920be9663ae308dff6eade86be5e6676ae5818
HerbCaudill <herb@devresults.com>
2020-09-26 16:51:19 +0200

    alligator contd

--- src/Alligator/Cube.tsx
+++ src/Alligator/Cube.tsx
@@ -1,75 +1,61 @@
-﻿import React, { useMemo, useRef, useState, useEffect, useCallback } from 'react'
-import { random } from 'lodash'
+﻿import { random } from 'lodash'
+import React, { useMemo, useRef, useState } from 'react'
 import { useFrame } from 'react-three-fiber'
+
+const colors = {
+  default: 0xf95b3c,
+  hover: 'yellow',
+  active: 'white',
+}
+
+const randomPosition = () => [random(-3, 3, true), random(-3, 3, true), random(-3, 3, true)]
 
 export const Cube: React.FC<any> = () => {
   const mesh = useRef<THREE.Mesh>()
-  const time = useRef(0)
 
   const [isHovered, setIsHovered] = useState(false)
   const [isActive, setIsActive] = useState(false)
 
+  const [activeTime, setActiveTime] = useState(0)
+
   // const isActiveRef = useRef(isActive)
 
-  // position
-  const position = useMemo(() => {
-    return [random(-3, 3, true), random(-3, 3, true), random(-3, 3, true)]
-  }, [])
+  // random position & rotation speed
+  const position = useMemo(() => randomPosition(), [])
+  const rotationSpeed = useMemo(() => random(0.1, 4, true), [])
 
-  // random time mod factor
-  const timeMod = useMemo(() => random(0.1, 4, true), [])
+  const color = isHovered ? colors.hover : isActive ? colors.active : colors.default
 
-  // color
-  const color = isHovered //
-    ? 0xe5d54d
-    : isActive
-    ? 0xf7e7e5
-    : 0xf95b3c
-
-  //useEffect of the activeState
-  // useEffect(() => {
-  //   isActiveRef.current = isActive
-  // }, [isActive])
-
-  // raf loop
   useFrame(() => {
     if (mesh.current) {
-      mesh.current.rotation.y += 0.01 * timeMod
-      // bounce up & down if
+      mesh.current.rotation.y += 0.01 * rotationSpeed
+      // bounce up & down if active
       if (isActive) {
-        time.current += 0.03
-        mesh.current.position.y = position[1] + Math.sin(time.current) * 0.4
+        setActiveTime(activeTime + 0.01)
+        mesh.current.position.y = position[1] + Math.sin(activeTime * 15) * 0.3
       }
     }
   })
-
-  // Events
-  const onHover = useCallback(
-    (e, value) => {
-      e.stopPropagation()
-      setIsHovered(value)
-    },
-    [setIsHovered]
-  )
-
-  const onClick = useCallback(
-    e => {
-      e.stopPropagation()
-      setIsActive(v => !v)
-    },
-    [setIsActive]
-  )
 
   return (
     <mesh
       ref={mesh}
       position={position as [number, number, number]}
-      onClick={e => onClick(e)}
-      onPointerOver={e => onHover(e, true)}
-      onPointerOut={e => onHover(e, false)}
+      onClick={e => {
+        e.stopPropagation()
+        setIsActive(v => !v)
+      }}
+      onPointerOver={e => {
+        e.stopPropagation()
+        setIsHovered(true)
+      }}
+      onPointerOut={e => {
+        e.stopPropagation()
+        setIsHovered(false)
+      }}
     >
-      <boxBufferGeometry attach="geometry" args={[0.047, 0.5, 0.29]} />
-      <meshStandardMaterial attach="material" color={color} roughness={0.6} metalness={0.1} />
+      <boxBufferGeometry attach="geometry" args={[0.3, 0.3, 0.3]} />
+      <meshStandardMaterial attach="material" color={color} roughness={0.39} metalness={0.5} />
     </mesh>
   )
 }
